55ce9a60e56fbb1effea9aea83b118abd254de89
= <=>
2024-06-25 15:40:44 +0530

    web act

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import Nav from "@/components/Nav";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "YourPortfolioo",
-  description: "Create your portfolio with YourPortfolio",
+  title: "Web Act",
+  description: "Create your Websites with Web Act",
 };
 
 export default function RootLayout({ children }) {
